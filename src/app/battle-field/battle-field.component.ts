33ac9b7a34b584c4360f583b83c3282ef84cdecb
MBouzeboudja <mahdibouzeboudja@yahoo.fr>
2020-02-20 17:07:02 +0100

    Fix who we retrieve query param on battle-field component

--- src/app/battle-field/battle-field.component.ts
+++ src/app/battle-field/battle-field.component.ts
@@ -1,9 +1,10 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 import {PokemonService} from '../shared/pokemon.service';
 import {Battle, Message} from '../shared/logic/battle';
 import {Pokemon} from '../shared/models/pokemon';
-import {forkJoin} from 'rxjs';
+import {forkJoin, Observable} from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-battle-field',
@@ -28,18 +29,24 @@ export class BattleFieldComponent implements OnInit {
   }
 
   private initPokemon(): void {
-    const pok1Observable = this.pokemonService.getPokemonById( parseInt(this.route.snapshot.paramMap.get('pokemonId1'), 10));
-    const pok2Observable = this.pokemonService.getPokemonById( parseInt(this.route.snapshot.paramMap.get('pokemonId2'), 10));
 
-    forkJoin([pok1Observable, pok2Observable]).subscribe(
+    let pok1Observable: Observable<Pokemon>;
+    let pok2Observable: Observable<Pokemon>;
+
+    this.route.params.pipe(mergeMap((params: Params) => {
+        pok1Observable = this.pokemonService.getPokemonById(params.pokemonId1);
+        pok2Observable = this.pokemonService.getPokemonById(params.pokemonId2);
+        return forkJoin([pok1Observable, pok2Observable]);
+      }
+    )).subscribe(
       result => {
         this.pokemon1 = result[0];
         this.pokemon2 = result[1];
         this.battle = new Battle(this.pokemon1, this.pokemon2);
+        this.battle.runFight();
       },
       error => console.log(`Error while trying to retrieve pokemon from pokemon service ${error}`),
-      () => { this.battle.runFight(); }
-      );
+    );
   }
 
 }
